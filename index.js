4f8f8a649054d495c333ea8b6abd7d51ea1c0c9d
jshindo <jshindo@gladd.jp>
2018-01-31 14:48:55 +0900

    evalからrequireに変更

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-/* eslint-disable no-console, no-eval */
+/* eslint-disable no-console */
 const fs = require('fs');
+const path = require('path');
 const colors = require('./colors.js');
 
+const TEMP_FILE = path.resolve('tmp.js');
 let timerId = 0;
 
 const doTest = () => {
-    console.log(`${new Date().toLocaleString()} ${'-'.repeat(60)}`);
+    const divider = `${new Date().toLocaleString()} ${'-'.repeat(60)}`;
+    const script = fs.readFileSync('./test.js', { encoding: 'UTF-8' });
+    fs.writeFileSync(TEMP_FILE, `module.exports = () => {${script}};`);
 
     try {
-        const script = fs.readFileSync('./test.js', { encoding: 'UTF-8' });
-        eval(script);
+        console.log(colors.bright);
+        console.log(divider, colors.reset);
+        require(TEMP_FILE)(); // eslint-disable-line global-require, import/no-dynamic-require
         console.log(colors.bgGreen, 'SUCCESS!', colors.reset);
     } catch (e) {
         console.log(colors.bgRed, 'ERROR!', colors.reset);
-        console.error(colors.red, e);
+        console.error(colors.yellow, e, colors.reset);
     }
 
-    console.log(colors.reset);
+    fs.unlinkSync(TEMP_FILE);
+    delete require.cache[TEMP_FILE];
 };
+
+doTest();
 
 fs.watch('./test.js', () => {
     clearTimeout(timerId);
