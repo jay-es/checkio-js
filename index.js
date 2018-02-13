4081c6a738626c924977ff9d5ce63c29001cddd1
jshindo <jshindo@gladd.jp>
2018-02-13 15:42:02 +0900

    インデント変更

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ const TEMP_FILE = path.resolve('tmp.js');
 let timerId = 0;
 
 const doTest = () => {
-    const divider = `${new Date().toLocaleString()} ${'-'.repeat(60)}`;
-    const script = fs.readFileSync(SCRIPT_FILE, { encoding: 'UTF-8' });
-    fs.writeFileSync(TEMP_FILE, `module.exports = () => {${script}\n};`);
+  const divider = `${new Date().toLocaleString()} ${'-'.repeat(60)}`;
+  const script = fs.readFileSync(SCRIPT_FILE, { encoding: 'UTF-8' });
+  fs.writeFileSync(TEMP_FILE, `module.exports = () => {${script}\n};`);
 
-    try {
-        console.log(colors.white);
-        console.log(divider, colors.default);
-        require(TEMP_FILE)(); // eslint-disable-line global-require, import/no-dynamic-require
-        console.log(colors.bgGreen, 'SUCCESS!', colors.bgDefault);
-    } catch (e) {
-        console.log(colors.bgRed, 'ERROR!', colors.bgDefault);
-        console.error(colors.yellow, e, colors.default);
-    }
+  try {
+    console.log(colors.white);
+    console.log(divider, colors.default);
+    require(TEMP_FILE)(); // eslint-disable-line global-require, import/no-dynamic-require
+    console.log(colors.bgGreen, 'SUCCESS!', colors.bgDefault);
+  } catch (e) {
+    console.log(colors.bgRed, 'ERROR!', colors.bgDefault);
+    console.error(colors.yellow, e, colors.default);
+  }
 
-    fs.unlinkSync(TEMP_FILE);
-    delete require.cache[TEMP_FILE];
+  fs.unlinkSync(TEMP_FILE);
+  delete require.cache[TEMP_FILE];
 };
 
 doTest();
 
 fs.watch(SCRIPT_FILE, () => {
-    clearTimeout(timerId);
-    timerId = setTimeout(doTest, 25);
+  clearTimeout(timerId);
+  timerId = setTimeout(doTest, 25);
 });
 
 // Object.keys(colors).forEach((k) => {
-//     console.log(`${colors[k]}%s${colors.reset}`, k);
+//   console.log(`${colors[k]}%s${colors.reset}`, k);
 // });
