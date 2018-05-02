42789de9c42ed86dffee5110d291890ecb27b1b1
Jun Shindo <jshindo@gladd.jp>
2018-05-02 21:49:00 +0900

    ESLintルール追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     indent: 0, // ["error", 4],
+    'no-continue': 0,
     'no-plusplus': [2, { "allowForLoopAfterthoughts": true }], // for文はOKにする
   }
 }
